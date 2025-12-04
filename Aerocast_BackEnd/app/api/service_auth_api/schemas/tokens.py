3f0076f15d55ddb9d06b5ef2f71e83e5d1ecf58b
tresor-del <tresoradzanto7@gmail.com>
2025-12-04 16:57:17 +0000

    Ajout du service d'authentication

---
+++ Aerocast_BackEnd/app/api/service_auth_api/schemas/tokens.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel, ConfigDict
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class TokenRead(BaseModel):
+    sub: str | None = None
+
+    model_config = ConfigDict(from_attributes=True) 
